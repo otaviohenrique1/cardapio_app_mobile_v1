28f2ae303e8dd886bc193527a450e2e7facf0caa
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-02-15 20:08:12 -0300

    aletrando tipo do ativo de string para boolean

--- src/utils/listas.ts
+++ src/utils/listas.ts
@@ -3,48 +3,48 @@ export const lista_teste_refeicoes = [
     id: 1,
     nome: 'X-burger 1',
     preco: 10,
-    ativo: 'ativo'
+    ativo: true
   },
   {
     id: 2,
     nome: 'X-burger 2',
     preco: 10,
-    ativo: 'ativo'
+    ativo: true
   },
   {
     id: 3,
     nome: 'X-burger 3',
     preco: 10,
-    ativo: 'ativo'
+    ativo: true
   },
   {
     id: 4,
     nome: 'X-burger 4',
     preco: 10,
-    ativo: 'ativo'
+    ativo: true
   },
   {
     id: 5,
     nome: 'X-burger 5',
     preco: 10,
-    ativo: 'ativo'
+    ativo: true
   },
   {
     id: 6,
     nome: 'X-burger 6',
     preco: 10,
-    ativo: 'inativo'
+    ativo: false
   },
   {
     id: 7,
     nome: 'X-burger 7',
     preco: 10,
-    ativo: 'inativo'
+    ativo: false
   },
   {
     id: 8,
     nome: 'X-burger 8',
     preco: 10,
-    ativo: 'inativo'
+    ativo: false
   },
 ];
