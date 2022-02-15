8c0e30b3b0b59f9358e650462cf72c9b1acce468
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-02-15 20:10:15 -0300

    alterando interface, adicionando componente Lista alterando styles e implementando codigo que pega dados de api

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,44 +1,31 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import { lista_teste_refeicoes } from '../../utils/listas';
-import { FormataValorMonetarioTexto } from '../../utils/utils';
+import { Alert, StyleSheet, View } from 'react-native';
+import { Lista } from '../../components/Lista';
+import api from '../../utils/api';
 
 interface DataTypes {
   id: number;
   nome: string;
   preco: number;
-  ativo: string;
+  ativo: boolean;
 }
 
 export function HomePage() {
   const [data, setData] = useState<DataTypes[]>([]);
   
   useEffect(() => {
-    setData(lista_teste_refeicoes);
+    api.get('/refeicoes')
+      .then((data) => {
+        setData(data.data);
+      })
+      .catch((erro) => {
+        Alert.alert('Erro', `${erro}`);
+      });
   }, []);
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={data}
-        renderItem={(item) => {
-          return (
-            <View style={styles.itemLista}>
-              <View style={styles.itemListaNome}>
-                <Text style={styles.itemListaTexto}>{item.item.nome}</Text>
-              </View>
-              <View style={styles.itemListaPrecoAtivo}>
-                <View style={styles.itemListaPrecoContainer}>
-                  <Text style={[styles.itemListaPrecoLabel, styles.itemListaTexto]}>Preço (R$):</Text>
-                  <Text style={styles.itemListaTexto}>{FormataValorMonetarioTexto(item.item.preco)}</Text>
-                </View>
-                <Text style={styles.itemListaTexto}>{item.item.ativo}</Text>
-              </View>
-            </View>
-          );
-        }}
-        keyExtractor={item => item.id.toString()}
-      />
+      <Lista data={data} />
     </View>
   );
 }
@@ -47,33 +34,4 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 10,
   },
-  itemLista: {
-    flexDirection: 'column',
-    marginBottom: 10,
-    padding: 10,
-    backgroundColor: 'lightskyblue',
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius: 15,
-  },
-  itemListaTexto: {
-    fontSize: 18,
-  },
-  itemListaNome: {
-    marginBottom: 5,
-    paddingBottom: 5,
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-  },
-  itemListaPrecoAtivo: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  itemListaPrecoContainer: {
-    flexDirection: 'row',
-  },
-  itemListaPrecoLabel: {
-    marginRight: 5,
-    fontWeight: 'bold',
-  }
 });
