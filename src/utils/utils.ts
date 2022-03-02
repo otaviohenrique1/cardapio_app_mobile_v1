2663a07d8eee9c1f0185f248ab80cdb5a8dbf492
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-01 22:13:05 -0300

    Adicionando função ConverteStringParaArrayObjetos2

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,7 @@
 export function FormataValorMonetarioTexto(valor: number) {
   return valor.toFixed(2).toString().replace('.', ',');
 }
+
+export function ConverteStringParaArrayObjetos2(texto: string) {
+  return JSON.parse(texto);
+}
