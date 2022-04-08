4611e00ab873e8e1776fbbc8b7db452f79d19435
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-08 18:47:43 -0300

    alterando baseURL

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: 'exp://57-b82.otaviohenls625.cardapio-app.exp.direct:80:3333'
   // exp://57-b82.otaviohenls625.cardapio-app.exp.direct:80
-  baseURL: 'http://192.168.0.11:3333'
+  // baseURL: 'http://192.168.0.11:3333'
+  baseURL: 'http://localhost:3333/'
   // exp://192.168.0.11:19000
   // baseURL: 'http://localhost:3333'
 });
