c1320f201ed7bb774c6b80ea8aa87c62458d7cfa
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-08 18:50:03 -0300

    adicionando sha512

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,13 @@
+import { SHA512, enc } from "crypto-js";
+
 export function FormataValorMonetarioTexto(valor: number) {
   return valor.toFixed(2).toString().replace('.', ',');
 }
 
 export function ConverteStringParaArrayObjetos2(texto: string) {
   return JSON.parse(texto);
+}
+
+export function sha512(senha: string) {
+  return SHA512(senha).toString(enc.Hex);
 }
