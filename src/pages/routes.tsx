d2ac2bbdf4ca4ce79685c7a84917877769319b0f
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-08 18:50:55 -0300

    adicionando rotas:  Login  NovoUsuario

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -2,8 +2,12 @@ import { Text, StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { HomePage } from "./HomePage";
 import { RefeicaoPage } from "./RefeicaoPage";
+import { Login } from "./Login";
+import { NovoUsuario } from "./NovoUsuario";
 
 export type RootStackParamList = {
+  Login: undefined;
+  NovoUsuario: undefined;
   HomePage: undefined;
   RefeicaoPage: { id: string };
 }
@@ -13,6 +17,16 @@ export function Routes() {
 
   return (
     <Stack.Navigator>
+      <Stack.Screen
+        name="NovoUsuario"
+        component={NovoUsuario}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen
         name="HomePage"
         component={HomePage}
