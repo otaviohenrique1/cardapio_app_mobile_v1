e0a3f4a00423c9f5cf4d25be799c02611992f477
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-14 23:06:12 -0300

    reformulando arquivo

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,11 +1,42 @@
-interface RefeicaoTypes {
-  nome: string;
-  preco: number;
-  ingredientes: { nome: string }[]
+/* Parte do Login */
+interface LoginTypes {
+  email: string;
+  senha: string;
 }
 
-interface ListaRefeicaoTypes {
+interface UsuarioLogadoTypes {
   id: string;
   nome: string;
+}
+
+/* Parte do Usuario */
+interface UsuarioTypes {
+  nome: string;
+  email: string;
+  senha: string;
+}
+
+interface UsuarioDadosTypes extends UsuarioTypes {
+  id: string;
+  codigo: string;
+  data_cadastro: string;
+  data_modificacao_cadastro: string;
+}
+
+/* Parte da Refeicao */
+interface RefeicaoBaseTypes {
+  nome: string;
   preco: number;
+}
+
+interface RefeicaoTypes extends RefeicaoBaseTypes {
+  ingredientes: Ingredientes[];
+}
+
+interface ListaRefeicaoTypes extends RefeicaoBaseTypes {
+  id: string;
+}
+
+interface Ingredientes {
+  nome: string;
 }
