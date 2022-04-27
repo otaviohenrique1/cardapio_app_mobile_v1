c8fa36baf4e59c325f7c189d41f6ebfcb5545eed
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-27 18:21:42 -0300

    reformulando arquivo

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import { Alert, FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Alert, FlatList, StyleSheet, Text, View, TouchableOpacity, ListRenderItemInfo } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../routes';
 import { Container } from '../../components/Container';
-import api, { id_empresa_cliente } from '../../utils/api';
+import { ApiBuscaDadosTodasRefeicoes, id_empresa_cliente } from '../../utils/api';
 import { FormatadorDados } from '../../utils/FormatadorDados';
 
 type NavigationProps = {
@@ -14,7 +14,125 @@ export function HomePage({ navigation }: NavigationProps) {
   const [data, setData] = useState<ListaRefeicaoTypes[]>([]);
 
   useEffect(() => {
-    api.get(`refeicao/cardapio/${id_empresa_cliente}`)
+    // api.get(`refeicao/cardapio/${id_empresa_cliente}`)
+    ApiBuscaDadosTodasRefeicoes(id_empresa_cliente)
+      .then((data) => {
+        let lista = [...data.data];
+        let listaFiltrada = lista.filter((item) => item.ativo !== false);
+        let validaLista = (listaFiltrada) ? listaFiltrada : [];
+        setData(validaLista);
+      })
+      .catch((erro) => {
+        Alert.alert('Erro', `${erro}`);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <ListaRefeicoes data={data} navigation={navigation} />
+    </Container>
+  );
+}
+
+interface ListaRefeicoesProps {
+  data: ListaRefeicaoTypes[];
+  navigation: NativeStackNavigationProp<RootStackParamList>;
+}
+
+function ListaRefeicoes(props: ListaRefeicoesProps) {
+  const { data, navigation } = props;
+
+  return (
+    <FlatList
+      data={data}
+      renderItem={(item) => <ItemListaRefeicoes item={item} navigation={navigation} />}
+      keyExtractor={item => item.id.toString()}
+    />
+  );
+}
+
+interface ItemListaRefeicoesProps {
+  item: ListRenderItemInfo<ListaRefeicaoTypes>;
+  navigation: NativeStackNavigationProp<RootStackParamList>;
+}
+
+function ItemListaRefeicoes(props: ItemListaRefeicoesProps) {
+  const { item, navigation } = props;
+  const { id, nome, preco } = item.item;
+  const preco_formatado = FormatadorDados.FormataValorMonetarioTexto(preco);
+  const { itemLista, itemListaNome, itemListaTexto, itemListaPrecoContainer, itemListaPrecoLabel } = styles;
+
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate('RefeicaoPage', { id })}>
+      <View style={itemLista}>
+        <View style={itemListaNome}>
+          <Text style={itemListaTexto}>{nome}</Text>
+        </View>
+        <View style={itemListaPrecoContainer}>
+          <Text style={[itemListaPrecoLabel, itemListaTexto]}>Preço (R$):</Text>
+          <Text style={itemListaTexto}>{preco_formatado}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemLista: {
+    flexDirection: 'column',
+    marginBottom: 15,
+    padding: 10,
+    backgroundColor: '#9dd7fb',
+    borderColor: '#6cc3f9',
+    borderWidth: 1,
+    borderRadius: 15,
+    shadowOffset: {
+      width: 2,
+      height: 6,
+    },
+    shadowRadius: 5,
+    shadowOpacity: 10,
+    shadowColor: 'lightgray'
+  },
+  itemListaTexto: {
+    fontSize: 25,
+  },
+  itemListaNome: {
+    marginBottom: 5,
+    paddingBottom: 5,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
+  itemListaPrecoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  itemListaPrecoLabel: {
+    marginRight: 5,
+    fontWeight: 'bold',
+  }
+});
+
+/*
+import { useEffect, useState } from 'react';
+import { Alert, FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../routes';
+import { Container } from '../../components/Container';
+import { ApiBuscaDadosTodasRefeicoes, id_empresa_cliente } from '../../utils/api';
+import { FormatadorDados } from '../../utils/FormatadorDados';
+
+type NavigationProps = {
+  navigation: NativeStackNavigationProp<RootStackParamList>;
+}
+
+export function HomePage({ navigation }: NavigationProps) {
+  const [data, setData] = useState<ListaRefeicaoTypes[]>([]);
+
+  useEffect(() => {
+    // api.get(`refeicao/cardapio/${id_empresa_cliente}`)
+    ApiBuscaDadosTodasRefeicoes(id_empresa_cliente)
       .then((data) => {
         let lista = [...data.data];
         let listaFiltrada = lista.filter((item) => item.ativo !== false);
@@ -33,7 +151,7 @@ export function HomePage({ navigation }: NavigationProps) {
       <FlatList
         data={data}
         renderItem={(item) => {
-          const {id, nome, preco } = item.item;
+          const { id, nome, preco } = item.item;
           const preco_formatado = FormatadorDados.FormataValorMonetarioTexto(preco);
 
           return (
@@ -92,4 +210,4 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   }
 });
-
+*/
