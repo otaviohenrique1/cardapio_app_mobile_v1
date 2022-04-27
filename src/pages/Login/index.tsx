de58a548493ff2bbd48de24fadec7f3f07e2a717
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-27 18:21:50 -0300

    reformulando arquivo

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -10,9 +10,9 @@ import { BotaoContainer } from "../../components/Container/BotaoContainer";
 import { Titulo } from '../../components/Titulo';
 import { TituloContainer } from "../../components/Container/TituloContainer";
 import { Subtitulo } from "../../components/Subtitulo";
-import { Botao } from '../../components/Botao';
-import { CampoInput } from '../../components/Campos/CampoInput';
-import api from '../../utils/api';
+import { Botao, BotaoProps } from '../../components/Botao';
+import { CampoInput, CampoInputProps } from '../../components/Campos/CampoInput';
+import api, { ApiBuscaLoginCliente, ApiBuscaLoginClienteTypes } from '../../utils/api';
 import { schemaValidacaoFormularioLogin } from '../../utils/ValidacaoSchemas';
 import { valoresIniciaisLogin } from '../../utils/constantes';
 import { FormatadorCrypto } from '../../utils/FormatadorCrypto';
@@ -32,17 +32,123 @@ export function Login({ navigation }: NavigationProps) {
     let senha_formatada = FormatadorCrypto.mensagemSHA512(senha);
     const data = { email, senha: senha_formatada };
     const auth = {
-      auth: {
-        username: email,
-        password: senha_formatada
-      }
+      username: email,
+      password: senha_formatada
     };
 
-    api.post('cliente/login', data, auth).
-      then((data) => {
+    const data_login: ApiBuscaLoginClienteTypes = { data, auth };
+    // api.post('cliente/login', data, { auth })
+    ApiBuscaLoginCliente(data_login)
+      .then((data) => {
         const { id, nome } = data.data.data_user;
         navigation.navigate('HomePage');
-      }).catch((error) => {
+      })
+      .catch((error) => {
+        const login_invalido = "Login inválido";
+        Alert.alert(login_invalido);
+        alert(login_invalido);
+        console.error(error);
+      });
+  };
+
+  const lista_dados_campos: CampoInputProps[] = [
+    { control: control, name: "email", erro: errors.email, placeholder: "Email", keyboardType: "email-address", secureTextEntry: false },
+    { control: control, name: "senha", erro: errors.senha, placeholder: "Senha", keyboardType: "default", secureTextEntry: true },
+  ];
+
+  const lista_dados_botoes: BotaoProps[] = [
+    { on_press: handleSubmit((onSubmit)), botao_texto_cor: 'white', botao_cor: 'blue', botao_texto: 'Entrar' },
+    { on_press: () => reset(), botao_texto_cor: 'white', botao_cor: 'red', botao_texto: 'Limpar' },
+    { on_press: () => navigation.navigate('NovoUsuario'), botao_texto_cor: 'white', botao_cor: 'green', botao_texto: 'Novo Usuario' }
+  ];
+
+  return (
+    <Container>
+      <TituloContainer>
+        <Titulo texto="Cardapio" />
+        <Subtitulo texto="Digital" />
+      </TituloContainer>
+      {lista_dados_campos.map((item, index) => {
+        const { control, name, erro, placeholder, keyboardType, secureTextEntry } = item;
+        return (
+          <CampoInput
+            key={index}
+            control={control}
+            name={name}
+            erro={erro && <MensagemErro menssagem={erro.message} />}
+            placeholder={placeholder}
+            keyboardType={keyboardType}
+            secureTextEntry={secureTextEntry}
+          />
+        );
+      })}
+      <BotaoContainer>
+        {lista_dados_botoes.map((item, index) => {
+          const { on_press, botao_texto_cor, botao_cor, botao_texto } = item;
+          return (
+            <Botao
+              key={index}
+              on_press={on_press}
+              botao_texto_cor={botao_texto_cor}
+              botao_cor={botao_cor}
+              botao_texto={botao_texto}
+            />
+          );
+        })}
+      </BotaoContainer>
+    </Container>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+/*
+import React from 'react';
+import { Alert, StyleSheet } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../routes';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { MensagemErro } from '../../components/MensagemErro';
+import { Container } from '../../components/Container';
+import { BotaoContainer } from "../../components/Container/BotaoContainer";
+import { Titulo } from '../../components/Titulo';
+import { TituloContainer } from "../../components/Container/TituloContainer";
+import { Subtitulo } from "../../components/Subtitulo";
+import { Botao } from '../../components/Botao';
+import { CampoInput } from '../../components/Campos/CampoInput';
+import api, { ApiBuscaLoginCliente, ApiBuscaLoginClienteTypes } from '../../utils/api';
+import { schemaValidacaoFormularioLogin } from '../../utils/ValidacaoSchemas';
+import { valoresIniciaisLogin } from '../../utils/constantes';
+import { FormatadorCrypto } from '../../utils/FormatadorCrypto';
+
+type NavigationProps = {
+  navigation: NativeStackNavigationProp<RootStackParamList>;
+}
+
+export function Login({ navigation }: NavigationProps) {
+  const { control, handleSubmit, formState: { errors }, reset } = useForm<LoginTypes>({
+    defaultValues: valoresIniciaisLogin,
+    resolver: yupResolver(schemaValidacaoFormularioLogin)
+  });
+
+  function onSubmit(values: LoginTypes) {
+    const { email, senha } = values;
+    let senha_formatada = FormatadorCrypto.mensagemSHA512(senha);
+    const data = { email, senha: senha_formatada };
+    const auth = {
+      username: email,
+      password: senha_formatada
+    };
+
+    const data_login: ApiBuscaLoginClienteTypes = { data, auth };
+    // api.post('cliente/login', data, { auth })
+    ApiBuscaLoginCliente(data_login)
+      .then((data) => {
+        const { id, nome } = data.data.data_user;
+        navigation.navigate('HomePage');
+      })
+      .catch((error) => {
         const login_invalido = "Login inválido";
         Alert.alert(login_invalido);
         alert(login_invalido);
@@ -96,3 +202,4 @@ export function Login({ navigation }: NavigationProps) {
 }
 
 const styles = StyleSheet.create({});
+*/
